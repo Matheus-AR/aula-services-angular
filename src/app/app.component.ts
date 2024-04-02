43df7ebe7f0e3631acfb73d158be3b854dfe07cc
Matheus-AR <matheusaraujorodrigues@outlook.com>
2024-04-02 16:40:06 -0300

    aula sobre services no angular finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonData } from '../../models/PokemonData';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
+  providers: [PokemonService],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  pokemon: PokemonData
+
+  constructor(
+    private service: PokemonService
+  ) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: ''
+      },
+      types: []
+    }
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+
+  getPokemon(searchName: string) {
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (res) => {
+
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+        },
+        error: (err) => console.log("not found")
+      }
+    )
+  }
 
 }
